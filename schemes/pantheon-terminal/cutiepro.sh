89f132dc0a79498ff70206553daa578e28377a6f
jezzy jumble <jezz@jezzy.dev>
2024-06-18 05:01:43 -0700

    Update all schemes using tools/gen.py

--- schemes/pantheon-terminal/cutiepro.sh
+++ schemes/pantheon-terminal/cutiepro.sh
@@ -2,8 +2,8 @@
 dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='cutiepro'
-cursor-color='#efc4cd'
+cursor-color='#e5a1a3'
 foreground='#d5d0c9'
 background='rgba(24,24,24,.95)'
-palette='#000000:#f56e7f:#bec975:#f58669:#42d9c5:#d286b7:#37cb8a:#d5c3c3:#88847f:#e5a1a3:#e8d6a7:#f1bb79:#80c5de:#b294bb:#9dccbb:#ffffff'
+palette='#000000:#f56e7f:#bec975:#f58669:#42d9c5:#d286b7:#37cb8a:#88847f:#2e2a27:#e5a1a3:#e8d6a7:#f3b061:#80c5de:#c3a6cb:#9ed7a8:#d7c3ba'
 COLORS
