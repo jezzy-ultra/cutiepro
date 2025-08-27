3582995ed1429b7d93b29d108cadcf197765e3a4
jezzy jumble <jezz@jezzy.dev>
2025-08-27 04:41:15 -0700

    remove old v1 code

--- schemes/pantheon-terminal/cutiepro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dconf load /org/pantheon/terminal/settings/ <<COLORS
-[/]
-name='cutiepro'
-cursor-color='#e5a1a3'
-foreground='#d5d0c9'
-background='rgba(24,24,24,.95)'
-palette='#000000:#f56e7f:#bec975:#f58669:#42d9c5:#d286b7:#37cb8a:#88847f:#2e2a27:#e5a1a3:#e8d6a7:#f3b061:#80c5de:#c3a6cb:#9ed7a8:#d7c3ba'
-COLORS
